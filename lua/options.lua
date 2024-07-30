16db07ebea86ac24d649946f17a5e0996ba816e4
Jiří Folprecht <folprjir@fit.cvut.cz>
2024-07-30 17:38:02 +0200

    Adding some options from kickstart

--- lua/options.lua
+++ lua/options.lua
@@ -1,9 +1,11 @@
 -- jiri Setting my options for Neovim.
 -- Include this file to main init.lua
+--
+-- options from primeagen -----------------------------------------------
 vim.opt.guicursor = ""
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.opt.nu = true -- line numbers
+vim.opt.relativenumber = false -- relative line numbers
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -31,3 +33,39 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+-- options from kickstart -------------------------------------------
+
+vim.g.have_nerd_font = false
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- ahoj
