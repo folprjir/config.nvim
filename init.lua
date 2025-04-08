04c17bcd1e0800f29d07e37401517460329d8a0d
Jiří Folprecht <folprjir@fit.cvut.cz>
2025-04-08 10:31:13 +0200

    Moving autocommands out of init.lua

--- init.lua
+++ init.lua
@@ -1,33 +1,8 @@
--- require("options")
-
--- require("autocommands")
 
 require("config.lazy")
 require("remaps")
-
-
--- vim.opt.number = true -- turns on line numbers
--- vim.opt.relativenumber = true
-
--- vim.opt.splitbelow = true -- new windows open at the bottom
--- vim.opt.splitright = true -- new splist open on the right side
-
-vim.opt.wrap = false -- disables line wrapping by default
-
--- vim.opt.expandtab = true -- chenges tabs to spaces
--- vim.opt.shiftwidth = 4 -- changes tabs to spaces on save
--- vim.opt.tabstop = 4 -- 
-
---vim.opt.clipboard = "unnamedplus" -- synchornizies clipboard with the systems clipboard
--- vim.opt.scrolloff = 999 -- keeps cursor in the middle
-
--- vim.opt.virtualedit = "block" -- makes working with block mode nicer
-
--- vim.opt.inccommand = "split" -- show macro previews in split
-
--- vim.opt.ignorecase = true -- ingores case in neovim
-
--- vim.opt.termguicolors = true -- enables 24 bit color support + nicer highlithing
+require("options")
+require("autocommands")
 
 
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
